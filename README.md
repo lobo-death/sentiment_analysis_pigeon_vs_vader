b5bf8d8727a3eadee413d5380f99f31a796b82b8
marcelo-machado-ds <mmpereira@paschoalotto.com.br>
2022-12-19 15:23:38 -0300

    readme modified and main created

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sentiment_analysis_pigeon_vs_vader
 Sentiment Analysis Pigeon vs Vader
+Test done in local laptop before git commit
---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import warnings
+import os
+
+warnings.simplefilter('ignore')
+
+os.listdir()
+
